829a0cc46acb7d1f749b0af5db4825a152478a6e
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-05-02 15:16:36 +0530

    adding sschema for database

--- models/listing.js
+++ models/listing.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema= new Schema({
-    title: String,
+    title:{
+        type:String,
+        require: true,
+    },
     description: String,
-    image: String,
+    image:{
+        type: String,
+        set:(v)=>v==="" ? "https://media.istockphoto.com/id/825319778/photo/sunset-on-beach.jpg?s=1024x1024&w=is&k=20&c=afSwJc6Q1aJFG26lPxSsDIx2aQU2ofsFGu9cGO6klkM=": v,
+    },
     price: Number,
     location: String,
     country: String,
 });
+
+const Listing = mongoose.model("Listing", listingSchema);
+modules.export=Listing;
