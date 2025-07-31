56d1f25516864395b296e94d5060e1c13c63f3f7
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-07-31 16:59:24 +0530

    creating schema for user

--- models/user.js
+++ models/user.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const passportlocalmongoose=require("passport-local-mongoose");
+
+const userSchema= new Schema({
+    email:{
+        type:String,
+        required: true
+        //we don't not required to write username and password 
+        //schema in user Schema because passport mongoose 
+        //already define it to the schema automatically when 
+        //schema created for database
+    }
+});
