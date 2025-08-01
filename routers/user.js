bd03658d6bfa39a1ef62d0695507e809a970b1a5
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-08-01 20:02:22 +0530

    adding wrapasync

--- routers/user.js
+++ routers/user.js
@@ -6,13 +6,13 @@ const wrapAsync = require("../utils/wrapAsync.js");
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
-router.post("/signup",wrapAsync(req,res)=>{ //we async with (req,res) because it make changes in database that can take time so to handle this we use async
+router.post("/signup",wrapAsync(async(req,res)=>{ //we async with (req,res) because it make changes in database that can take time so to handle this we use async
     let {username,email,password}=req.body;
     const newUser= new User({email,username});
     const registerUser= await User.register(newUser,password);
     req.flash("success","Welcome to GlobeTreker");
     res.redirect("/listings");
-});
+}));
 
 
 module.exports=router;
