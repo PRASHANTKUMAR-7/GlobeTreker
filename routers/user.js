754d3e7c9dbf5e388192df58829d3847764f9a74
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-08-01 22:13:03 +0530

    login route

--- routers/user.js
+++ routers/user.js
@@ -2,7 +2,7 @@ const express= require("express");
 const router= express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-
+const passport= require("passport")
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
@@ -20,6 +20,19 @@ router.post("/signup",wrapAsync(async(req,res)=>{ //we async with (req,res) beca
 
     }
 }));
+
+//login route
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+router.post("/login",
+    passport.authenticate("local",
+        {failureRedirect: '/login',
+            failureFlash: true}),
+            async(req,res)=>{
+                res.send("Welcome To GlobeTreker");
+
+});
 
 
 module.exports=router;
