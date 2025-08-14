1b06405c8c4e0eace434941991bc932867ebce01
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-08-14 14:53:30 +0530

    post login method

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isLoggedIn=(req,res,next)=>{
 if(!req.isAuthenticated()){
+    req.session.redirectUrl=req.originaUrl; //redired the user from when it canme to login
         req.flash("error","You must be logged in to create listing");
         return res.redirect("/login");
     }
