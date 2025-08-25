169cbd87c41c25fa5c90bd61b8d3df6833f6c2d5
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-08-25 15:00:57 +0530

    using mvc for user and requiring it

--- routers/user.js
+++ routers/user.js
@@ -4,11 +4,12 @@ const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport= require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
+const userController= require("../controller/user.js");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
-router.post("/signup",wrapAsync());
+router.post("/signup",wrapAsync(userController.signUp));
 
 //login route
 router.get("/login",(req,res)=>{
