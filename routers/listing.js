b9164c8292ec1b7987c788adaa4691330722c8b7
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-09-14 01:06:35 +0530

    adding multer and it saving file in local folder

--- routers/listing.js
+++ routers/listing.js
@@ -1,12 +1,21 @@
 const express= require("express");
 const router= express.Router();
+
 const wrapAsync=require("../utils/wrapAsync.js"); //client side custome error handling
 const Listing = require("../models/listing.js"); // lisiting mongodb Schema
 const {isLoggedIn, isOwner,validateListing}=require("../middleware.js");
 const ListingController=require("../controller/listings.js")
+const multer = require('multer');//multer is user to parse form data
+const upload = multer({dest: 'uploads/'});//after parsing form data save to uploads folder
 
+router
 //print all data on root route or It is Index Route
-router.get("/",wrapAsync(ListingController.index) //index is a the variable used here as refrence of function which is in listings file of controller folder
+    .route("/")
+    .get(wrapAsync(ListingController.index) //index is a the variable used here as refrence of function which is in listings file of controller folder
+    )
+    .post(upload.single("listing[image]"),(req,res)=>{
+    res.send(req.file);
+    }
 );
 
 //Route to Create new Listing
@@ -14,10 +23,13 @@ router.get("/new", isLoggedIn, wrapAsync(ListingController.renderNewForm));
 
 //Route to save new data created by above route 
 //there are two method to get data inserted in form either by targeting each data like this := let{title, description,image,price,country, location}=req.body but it is long method we can do it simmple way just make all data in new.ejs obj of listing watch in new.ejs 
-router.post("/",isLoggedIn, 
-    validateListing, //first check the validateListing then all 
-    wrapAsync(ListingController.createListing)
-);  
+// router.post("/",isLoggedIn, 
+//     validateListing, //first check the validateListing then all 
+//     wrapAsync(ListingController.createListing)
+// );  
+
+
+
 
 //Show Route
 router.get("/:id",  wrapAsync(ListingController.showRoute));
