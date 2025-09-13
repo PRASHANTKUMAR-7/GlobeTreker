843fd504f655ca189cab4d09443cb4e69d5f5a35
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-09-14 01:57:23 +0530

    adding cloudinary config

---
+++ cloudConfig.js
@@ -0,0 +1,16 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.CLOUD_API_KEY,
+    api_secret:process.env.CLOUD_API_SECRET,
+});//CONFIURING THE CLOUDINARY MEANS ADDING/CONNECTING CLOUDINARY TO BACKEND 
+
+//defining storage for cloudinary
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'globetreker_webdev',
+    allowerdFormats:["png","jpg","jpeg"] ,// supports promises as well
+  },
+});
